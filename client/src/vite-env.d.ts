5dd83675277de1629357a15f9da84b59c9852342
isahbella007 <okorofth@gmail.com>
2024-10-18 11:11:37 +0100

    correct api url for client

--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -1,3 +1,3 @@
 /// <reference types="vite/client" />
-// VITE_API_URL='https://deep-tech-ifbo.onrender.com/'
-VITE_API_URL='http://localhost:3000/'
+VITE_API_URL='https://deep-tech-ifbo.onrender.com/'
+// VITE_API_URL='http://localhost:3000/'
